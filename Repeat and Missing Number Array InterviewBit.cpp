cd21a92355453d0d40547eb9d5e4c1f52dcacab4
lirajkhanna <lirajkhanna2002@gmail.com>
2022-12-21 00:40:05 +0530

    Bit manipulation problem

--- Repeat and Missing Number Array InterviewBit.cpp
+++ Repeat and Missing Number Array InterviewBit.cpp
@@ -1,3 +1,67 @@
+// Best apporach 
+// bit manipulation
+// TC=>O(N) SC=>O(1)
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        int* out=new int[2];
+        int x=0;
+        for(int i=0;i<n;i++){
+            x^=arr[i];
+            x^=(i+1);
+        }
+
+        int x0=0,x1=0;
+        int lastbit=(x&~(x-1));
+        
+        for(int i=0;i<n;i++){
+            if((arr[i]&lastbit)){
+                x0^=arr[i];
+            }else{
+                x1^=arr[i];
+            }
+            if(((i+1)&lastbit)){
+                x0^=(i+1);
+            }else{
+                x1^=(i+1);
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(x0==arr[i]) break;
+            if(x1==arr[i]){
+                swap(x0,x1);
+                break;
+            }
+        }
+        
+        out[0]=x0;
+        out[1]=x1;
+        return out;
+    }
+};
+
+// Using the sign trick modifying input array
+// TC=>O(N) SC => O(1)
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        int* out = new int[2];
+        for(int i=0;i<n;i++){
+            if(arr[abs(arr[i])-1]<0){
+                out[0]=abs(arr[i]);
+            }else{
+                arr[abs(arr[i])-1]*=-1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>0) out[1]=i+1;
+        }
+        return out;
+    }
+};
+
+
 // Using the formulaes of natual sum and sqaure natural sum
 // TC => O(N)  SC=>O(1)
 // But power could go large, so a downside
